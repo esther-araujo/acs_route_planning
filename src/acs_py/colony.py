551daaf180296429a62778ced5d71745b6aac6e9
EstherWI <esthersss09@gmail.com>
2023-08-24 22:43:33 -0300

    local pheromone update

--- src/acs_py/colony.py
+++ src/acs_py/colony.py
@@ -5,7 +5,7 @@ from ant import Ant
 ants = []
 
 class AntColonySystem:
-    def __init__(self, graph, num_ants, num_iterations, alpha, beta, rho, q0):
+    def __init__(self, graph, num_ants, num_iterations, alpha, beta, rho, q0, rho_local, tau_0_local):
         self.graph = graph
         self.num_nodes = graph.number_of_nodes()
         self.num_ants = num_ants
@@ -14,6 +14,9 @@ class AntColonySystem:
         self.beta = beta
         self.rho = rho
         self.q0 = q0
+
+        self.rho_local = rho_local
+        self.tau_0_local = tau_0_local
         
         self.pheromone = np.ones((self.num_nodes, self.num_nodes))
         
@@ -45,8 +48,11 @@ class AntColonySystem:
         
         return next_node
 
-        
+    # Fazer modificações para ACS (esta em AS)
     def update_pheromone(self, ant):
+        #Evaporate
+        self.evaporate_pheromones()
+        
         tour_length = ant.total_distance
         
         # Calculate the amount of pheromone to deposit on each edge
@@ -58,23 +64,31 @@ class AntColonySystem:
             
             # Update pheromone level on the edge (from_node, to_node)
             self.pheromone[from_node][to_node] += pheromone_deposit
-            self.pheromone[to_node][from_node] += pheromone_deposit  # Assuming an undirected graph
+            self.pheromone[to_node][from_node] += pheromone_deposit  
+
+    # Local Pheromone Update for ACS
+    def local_pheromone_update(self, from_node, to_node):
+        self.pheromone[from_node][to_node] = (1-rho_local)*self.pheromone[from_node][to_node]+(self.rho_local*self.tau_0_local)
+        self.pheromone[to_node][from_node] = (1-rho_local)*self.pheromone[from_node][to_node]+(self.rho_local*self.tau_0_local)
 
     def evaporate_pheromones(self):
         self.pheromone *= (1 - self.rho)
 
     def construct_solutions(self, ants):
         # Perform ant tours and update pheromones
+        best_solution = []
+        best_distance = float('inf')
         for ant in ants:
             while not ant.has_visited_all_nodes():
                 next_node = self.select_next_node(ant)
                 ant.visit(next_node)
-            
+                # Always update local pheromone after an ant moves to the next node
+                self.local_pheromone_update(ant.visited_nodes[-2], next_node) 
+
             if ant.total_distance < best_distance:
                 best_solution = ant.get_visited_nodes()
                 best_distance = ant.get_total_distance()
             
-            # Update pheromones using update_pheromone method
             self.update_pheromone(ant)
             
             # Reset ant for the next iteration
@@ -92,6 +106,9 @@ class AntColonySystem:
             #ComputeNearestNeighborLists
             # .........
 
+            #ComputeChoiceInformation
+            # .........
+
             #InitializeAnts
             ants = []
 
@@ -103,38 +120,33 @@ class AntColonySystem:
             best_solution = []
             best_distance = float('inf')
 
-            #InitializeParameters
-
-            #InitializeStatistics
-
-
             # Pode ser num de iterações, tempo limite de cpu, encontrou uma solução aceitavel dentro de um limite especificado, 
             # algoritmo demonstrou estagnação, etc
-            for iteration in range(self.num_iterations):
+            for _ in range(self.num_iterations):
 
                 #ConstructSolutions
                 best_solution, best_distance = self.construct_solutions(ants) 
 
                 #LocalSearch
-
-                #UpdateStatistics               
-                # Evaporate pheromones using evaporate_pheromones method
-                self.evaporate_pheromones()
+                # .........
 
             return best_solution, best_distance
 
 # Example usage
 G = nx.Graph()
-G.add_weighted_edges_from([(0, 1, 200), (0, 2, 202), (0, 3, 212), (1, 2, 125), (1, 3, 212), (2, 3, 40)])
+G.add_weighted_edges_from([(0, 1, 200), (0, 2, 202), (0, 3, 212), (1, 3, 212), (2, 3, 40)])
 
 num_ants = 10
 num_iterations = 100
 alpha = 1.0
 beta = 2.0
 rho = 0.1
+rho_local = 0.1
+tau_0_local = 0.1
 q0 = 0.9
 
-acs = AntColonySystem(G, num_ants, num_iterations, alpha, beta, rho, q0)
+acs = AntColonySystem(G, num_ants, num_iterations, alpha, beta, rho, q0, rho_local, tau_0_local)
 best_solution, best_distance = acs.run()
+
 print("Best solution:", best_solution)
 print("Best distance:", best_distance)
