f6195dd91250456e8a78ff87bec2f9c436543d5c
EstherWI <esthersss09@gmail.com>
2023-09-21 16:26:42 -0300

    generated maps

--- src/jody_map_creator/generate_maps.py
+++ src/jody_map_creator/generate_maps.py
@@ -2,10 +2,27 @@ from PIL import Image, ImageDraw
 import os
 import yaml
 import shutil
+import os
+import shutil
+import re
 
 # Pasta com o gerador de grafos de voronoi
 path_multi_robot = "/home/esther/catkin_ws/src/tuw_multi_robot/tuw_multi_robot_demo"
 path_route_planning = "/home/esther/catkin_ws/src/acs_route_planning"
+path_jody_maps = "/home/esther/TCC/random-rect/maps/result1"
+
+# Loop pelos arquivos no diretório de origem
+for arquivo in os.listdir(path_jody_maps):
+    if arquivo.endswith('.map'):
+        # Constrói o caminho completo do arquivo de origem e destino
+        caminho_origem = os.path.join(path_jody_maps, arquivo)
+        caminho_destino = os.path.join(path_route_planning+"/src/jody_map_creator/maps", arquivo)
+
+        # Copia o arquivo
+        shutil.copy(caminho_origem, caminho_destino)
+
+print("Arquivos .map copiados com sucesso!")
+
 num_maps = 1
 
 
@@ -40,35 +57,37 @@ map_yaml = {
 # Rviz config
 map_rviz = f"{path_route_planning}/src/map_creator/map.rviz"
 # Read the .map file
-# Replace 'your_map_file.map' with the actual file path
-with open('map.map', 'r') as file:
-    map_data = file.read().splitlines()
 
-# Initialize a list to store line coordinates
-rectangles = []
 
-# Flag to indicate whether we are currently reading lines
-reading_lines = False
+def generate_island_map(filename):
 
-# Iterate through the lines in the map data
-for line in map_data:
-    # Check if we've reached the "LINES" section
-    if line.strip() == "LINES":
-        reading_lines = True
-        continue  # Skip the "LINES" line itself
+    # Replace 'your_map_file.map' with the actual file path
+    with open(path_route_planning+"/src/jody_map_creator/maps/"+filename+".map", 'r') as file:
+        map_data = file.read().splitlines()
 
-    # Check if we've reached the end of the lines section
-    if reading_lines and line.strip() == "DATA":
-        break
+        # Initialize a list to store line coordinates
+        rectangles = []
 
-    # If we are in the lines section, parse the line coordinates
-    if reading_lines:
-        line_parts = line.split()
-        if len(line_parts) == 4:
-            x1, y1, x2, y2 = map(int, line_parts)
-            rectangles.append((x1, y1, x2, y2))
+        # Flag to indicate whether we are currently reading lines
+        reading_lines = False
 
-def generate_island_map():
+        # Iterate through the lines in the map data
+        for line in map_data:
+            # Check if we've reached the "LINES" section
+            if line.strip() == "LINES":
+                reading_lines = True
+                continue  # Skip the "LINES" line itself
+
+            # Check if we've reached the end of the lines section
+            if reading_lines and line.strip() == "DATA":
+                break
+
+            # If we are in the lines section, parse the line coordinates
+            if reading_lines:
+                line_parts = line.split()
+                if len(line_parts) == 4:
+                    x1, y1, x2, y2 = map(int, line_parts)
+                    rectangles.append((x1, y1, x2, y2))
 
     # Create a white background image
     img = Image.new("RGB", (map_width, map_height), "white")
@@ -94,34 +113,38 @@ def generate_island_map():
     # Save the image as a PNG file
     return img
 
-for i in range(num_maps):
-    
-    mapa = generate_island_map()
+files = os.listdir(path_route_planning+"/src/jody_map_creator/maps")
 
-    if not os.path.exists(f"{path_multi_robot}/cfg/maps/map{i + 1}"):
-        os.mkdir(f"{path_multi_robot}/cfg/maps/map{i + 1}")
-    if not os.path.exists(f"{path_multi_robot}/cfg/graph/map{i + 1}"):
-        os.mkdir(f"{path_multi_robot}/cfg/graph/map{i + 1}")
+# Loop through the files
+for filename in files:
+    filename = re.sub(r'\.map$', '', filename)
+    print(filename)
+    mapa = generate_island_map(filename)
+
+    if not os.path.exists(f"{path_multi_robot}/cfg/maps/{filename}"):
+        os.mkdir(f"{path_multi_robot}/cfg/maps/{filename}")
+    if not os.path.exists(f"{path_multi_robot}/cfg/graph/{filename}"):
+        os.mkdir(f"{path_multi_robot}/cfg/graph/{filename}")
     
     # PGM
-    pgm_filename = f"{path_multi_robot}/cfg/maps/map{i + 1}/map.pgm"
+    pgm_filename = f"{path_multi_robot}/cfg/maps/{filename}/map.pgm"
     mapa_gray = mapa.convert("L")
     mapa_gray.save(pgm_filename)
 
     # Map yaml
-    file_path = f"{path_multi_robot}/cfg/maps/map{i + 1}/map.yaml"
+    file_path = f"{path_multi_robot}/cfg/maps/{filename}/map.yaml"
 
     with open(file_path, 'w') as file:
         yaml.dump(map_yaml, file)
 
     # Graph yaml
-    file_path = f"{path_multi_robot}/cfg/graph/map{i + 1}/graph.yaml"
+    file_path = f"{path_multi_robot}/cfg/graph/{filename}/graph.yaml"
 
     with open(file_path, 'w') as file:
         yaml.dump(graph_yaml, file)
 
     # Rviz config
-    rviz_voronoi = f"{path_multi_robot}/cfg/rviz/map{i+1}.rviz" 
+    rviz_voronoi = f"{path_multi_robot}/cfg/rviz/{filename}.rviz" 
 
     shutil.copy(map_rviz, rviz_voronoi)
 
