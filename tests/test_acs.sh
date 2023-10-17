0227bce7e12abda9e12d7db958ddd02aa73e302c
EstherWI <esthersss09@gmail.com>
2023-10-17 12:35:21 -0300

    automatize tests

--- tests/test_acs.sh
+++ tests/test_acs.sh
@@ -6,6 +6,8 @@ directory="/home/esther/catkin_ws/src/acs_route_planning/scripts/jody_map_creato
 
 rm -rf "/home/esther/catkin_ws/src/acs_route_planning/tests/acs_logs"/*
 
+log_directory="/home/esther/catkin_ws/src/acs_route_planning/tests/acs_logs"
+
 # Loop through files in the directory
 for file in "$directory"/*; do
     # Check if the file is a regular file
@@ -14,6 +16,19 @@ for file in "$directory"/*; do
         filename=$(basename "$file" | cut -d. -f1)
         # Run your command with the filename as an argument
         echo "Processing file: $filename"
-        roslaunch acs_route_planning test_acs_ambient.launch room:="$filename"
+        process_name="roslaunch acs_route_planning test_acs_ambient.launch room:="$filename""
+        
+        gnome-terminal -- bash -c "$process_name"
+
+        # Loop para verificar a existência do arquivo de log
+        while true; do
+            if [ -e "$log_directory/$filename.log" ]; then
+                echo "Arquivo de log detectado. Fechando o terminal."
+                pkill -2 "$process_name"
+                sleep 1
+                break
+            fi
+            sleep 2
+        done
     fi
 done
